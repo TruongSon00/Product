a54974b78ad8260ced4da03e318c54c4ee64f3a8
truongson00 <soi30082000@gmail.com>
2023-10-10 09:41:32 +0700

    update full creen

--- clock/javascript.js
+++ clock/javascript.js
@@ -1,27 +1,31 @@
 const hour = 30,
-    minute = 6,
-    second = 6;
-
+  minute = 6,
+  second = 6;
 
 function clock() {
-      var t = new Date()
-    var hours = t.getHours()
-    var minutes = t.getMinutes()
-    var seconds = Math.floor(t.getSeconds())
-    hours = hours + minutes / 60
-    minutes = minutes + seconds / 60
-    $('.clock .tam .hour')[0].style.transform = `rotate(${hours * 30}deg )`
-    $('.clock .tam .minute')[0].style.transform = `rotate(${minutes * 6}deg )`
-    $('.clock .tam .second')[0].style.transform = `rotate(${seconds * 6}deg )`
+  var t = new Date();
+  var hours = t.getHours();
+  var minutes = t.getMinutes();
+  var seconds = Math.floor(t.getSeconds());
+  hours = hours + minutes / 60;
+  minutes = minutes + seconds / 60;
+  $(".clock .tam .hour")[0].style.transform = `rotate(${hours * 30}deg )`;
+  $(".clock .tam .minute")[0].style.transform = `rotate(${minutes * 6}deg )`;
+  $(".clock .tam .second")[0].style.transform = `rotate(${seconds * 6}deg )`;
+}
+
+function openFullscreen(button) {
+  document.getElementById("body").requestFullscreen();
+  button.style.display = "none";
 }
 
 function audioPlay() {
-    $('#myAudio')[0].autoPlay = true
-    $('#myAudio')[0].load()
+  $("#myAudio")[0].autoPlay = true;
+  $("#myAudio")[0].load();
 }
 setInterval(() => {
-    audioPlay()
+  audioPlay();
 }, 60000);
-setInterval(function() {
-    clock()
-}, 1000)
+setInterval(function () {
+  clock();
+}, 1000);
